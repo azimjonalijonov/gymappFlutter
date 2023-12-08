dbd558cbc701dc1ab21f4337004e0efac68a8bdd
mizamov <149356391+mizamov@users.noreply.github.com>
2023-12-08 16:19:41 +0500

    Update LoginSignupScreen.dart Mirzohid

--- lib/LoginSignupScreen.dart
+++ lib/LoginSignupScreen.dart
@@ -39,3 +39,86 @@ class LoginSignupScreen extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// class LoginSignupScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Welcome Back to GYM'),
+//         backgroundColor: Colors.transparent,
+//         elevation: 0,
+//       ),
+//       extendBodyBehindAppBar: true,
+//       body: Center(
+//         child: Card(
+//           margin: EdgeInsets.all(16.0),
+//           elevation: 8.0,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(16.0),
+//           ),
+//           child: Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               children: [
+//                 Text(
+//                   'Sign In',
+//                   style: TextStyle(
+//                     fontSize: 24.0,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 SizedBox(height: 16.0),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Email',
+//                     prefixIcon: Icon(Icons.email),
+//                   ),
+//                 ),
+//                 SizedBox(height: 16.0),
+//                 TextField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     prefixIcon: Icon(Icons.lock),
+//                   ),
+//                 ),
+//                 SizedBox(height: 24.0),
+//                 ElevatedButton(
+//                   onPressed: () {},
+//                   child: Text('Sign In'),
+//                   style: ElevatedButton.styleFrom(
+//                     primary: Color(0xff2196f3),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(10.0),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 16.0),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     TextButton(
+//                       onPressed: () {
+//                         Navigator.pushNamed(context, 'signup');
+//                       },
+//                       child: Text('Sign Up'),
+//                     ),
+//                     TextButton(
+//                       onPressed: () {},
+//                       child: Text('Forgot Password'),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
