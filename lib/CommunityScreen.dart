430f6c86120bb44a0c503206ec4d9cbef7bb5513
SardorEslamasov <eslamasovsardor@gmail.com>
2024-01-23 17:44:35 +0000

    Committed

--- lib/CommunityScreen.dart
+++ lib/CommunityScreen.dart
@@ -1,54 +1,82 @@
 import 'package:flutter/material.dart';
 
-class CommunityScreen extends StatelessWidget {
+class Message {
+  final String owner;
+  final String text;
+
+  Message(this.owner, this.text);
+}
+
+class CommunityScreen extends StatefulWidget {
+  @override
+  _CommunityScreenState createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  List<Message> messages = [
+    Message('Abror', 'Hey, everyone! How\'s your workout going?'),
+    Message('Bobur', 'I just finished a great session. Feeling energized!'),
+    Message('Jahongir', 'Any suggestions for a good ab workout?'),
+    Message('Komil', 'Excited to join the fitness challenge!'),
+  ];
+
+  TextEditingController messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Community'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // List of Sample Messages
-            _buildMessage('Abror', 'Hey, everyone! How is your day going?'),
-            _buildMessage('Bobur',
-                'I just finished a great workout session. Feeling energized! 💪'),
-            _buildMessage(
-                'Jahongir', 'Any recommendations for a good book to read?'),
-            _buildMessage('Abbos',
-                'Excited about the upcoming community event! Who else is joining? 🎉'),
-            // Add more messages as needed
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title:
+                      Text('${messages[index].owner}: ${messages[index].text}'),
+                  // Add more details like timestamp, etc., as needed
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Type your message...',
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    _sendMessage();
+                  },
+                  child: Text('Send'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildMessage(String userName, String message) {
-    return Card(
-      elevation: 2.0,
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              userName,
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              message,
-              style: TextStyle(fontSize: 14.0),
-            ),
-          ],
-        ),
-      ),
-    );
+  void _sendMessage() {
+    String messageText = messageController.text.trim();
+    if (messageText.isNotEmpty) {
+      setState(() {
+        messages.add(Message('Me', messageText));
+        messageController.clear();
+      });
+    }
   }
 }
 
